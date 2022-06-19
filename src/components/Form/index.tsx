ac4cd4eb7e1052298de25a45ef038f87dc7e2b0f
Gerson Rocha <gersonrocha9@gmail.com>
2022-06-19 12:49:26 -0300

    Funcionalidade de salvamento dos dados digitados pelo usuário implementada.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 import { Button, Container, FormContainer, Input, Title } from "./styles";
@@ -15,12 +16,24 @@ export const Form = ({ title, buttonText, onSubmit }: FormProps) => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     if (name === "" || email === "") {
-      alert("Por favor, preencha todos os campos obrigatórios");
+      alert("Preencha todos os campos");
     } else {
-      console.log(name);
-      console.log(email);
-      console.log(phone);
+      axios
+        .post("http://localhost:3000/subs", {
+          id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15), // ID aleatório
+          name,
+          email,
+          phone,
+        })
+        .then((response: { data: { id: string; name: string; email: string; phone: string } }) => {
+          alert("Cadastro realizado com sucesso");
+        })
+        .catch((error: { response: { data: { message: string } } }) => {
+          console.log(error);
+          alert("Erro ao realizar o cadastro");
+        });
     }
   };
 
