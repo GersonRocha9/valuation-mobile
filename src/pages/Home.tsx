a0df1579e28dc2645d41a23d4dc4679629442132
Gerson Rocha <gersonrocha9@gmail.com>
2022-07-17 12:01:18 -0300

    Corrigindo erro no caminho do arquivo de imagem como bg no componente Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ export const Container = styled.div`
   margin: 0 auto;
   width: 375px;
   padding: 1.875rem;
-  background-image: url("../../../src/assets/images/bg.png");
+  background-image: url("../src/assets/images/bg.png");
   background-size: contain;
   background-repeat: no-repeat;
   background-position: top;
